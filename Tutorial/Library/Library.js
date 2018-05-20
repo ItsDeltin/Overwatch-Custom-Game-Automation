17530ca595e7e71d6a52d2c6fa34658da9c44409
Deltin <grittydeltin@gmail.com>
2018-05-20 10:04:05 -0800

    Tutorial updates

---
+++ Tutorial/Library/Library.js
@@ -0,0 +1,19 @@
+﻿function getSidebar(page) {
+    var req = new XMLHttpRequest();
+
+    req.onload = function () {
+        document.getElementsByClassName("sidebar")[0].innerHTML = this.responseText.replace(">" + page + "<", "><strong>" + page + "</strong><");
+    }
+
+    var path = window.location.href.split("/");
+    var sidebarPage = "";
+    for (var p = path.length - 2; p >= 0; p--) {
+        if (path[p] == "Library")
+            break;
+        sidebarPage += "../";
+    }
+    sidebarPage += "Sidebar.html";
+
+    req.open('GET', sidebarPage);
+    req.send();
+}
